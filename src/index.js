5b117848f1ed323ddb490d556ffff78093379a92
Shirish Koirala <shirish.mjpro@gmail.com>
2025-03-20 09:31:06 +1000

    Fixed all the issues regarding react-scripts update

--- src/index.js
+++ src/index.js
@@ -1,30 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
+import {AppRegistry} from 'react-native';
 import App from './App';
-import './index.css';
 
-import * as ReactDOMLegacy from 'react-dom';
-import AppRoot from './util/AppRoot';
-
-if (!ReactDOMLegacy.findDOMNode) {
-  ReactDOMLegacy.findDOMNode = (component) => {
-    if (component) {
-      if (component instanceof HTMLElement) {
-        return component;
-      }
-      if (component.base) {
-        return component.base;
-      }
-    }
-    console.warn('findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. You can use a ref to get the DOM node.');
-    return null;
-  };
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <AppRoot>
-    <App />
-  </AppRoot>
-);
+AppRegistry.registerComponent('App', () => App);
+AppRegistry.runApplication('App', {
+  rootTag: document.getElementById('root'),
+});
