75acd86e08f71f96389814d341a51297eb8df864
Shirish Koirala <shirish.mjpro@gmail.com>
2025-03-24 11:49:25 +1000

    Hex Converted to Base64

--- src/util/crypto.js
+++ src/util/crypto.js
@@ -6,13 +6,13 @@ const { SECRET_KEY } = require('./constants');
 const decrypt = (data) => {
     var parts = data.split(':');
     if (parts.length !== 2) {
-        console.error('Unexpected format for encrypted data. Expected "ivHex:ciphertextHex".');
+        console.error('Unexpected format for encrypted data. Expected "iv:ciphertext".');
     } else {
-        var ivHex = parts[0];
-        var ciphertextHex = parts[1];
+        var ivBase64 = parts[0];
+        var ciphertextBase64 = parts[1];
 
-        var iv = CryptoJS.enc.Hex.parse(ivHex);
-        var ciphertext = CryptoJS.enc.Hex.parse(ciphertextHex);
+        var iv = CryptoJS.enc.Base64.parse(ivBase64);
+        var ciphertext = CryptoJS.enc.Base64.parse(ciphertextBase64);
 
         var cipherParams = CryptoJS.lib.CipherParams.create({
             ciphertext: ciphertext
@@ -22,7 +22,7 @@ const decrypt = (data) => {
         if (!SECRET_KEY) {
             console.error("The secret_key environment variable is not set.");
         } else {
-            var key = CryptoJS.enc.Hex.parse(SECRET_KEY);
+            var key = CryptoJS.enc.Base64.parse(SECRET_KEY);
 
             var decrypted = CryptoJS.AES.decrypt(cipherParams, key, { iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 });
 
@@ -46,7 +46,7 @@ const encrypt = (data) => {
         console.error("The secret_key environment variable is not set.");
     } else {
 
-        const key = CryptoJS.enc.Hex.parse(SECRET_KEY);
+        const key = CryptoJS.enc.Base64.parse(SECRET_KEY);
         const iv = CryptoJS.lib.WordArray.random(16);
 
         const encrypted = CryptoJS.AES.encrypt(json, key, {
@@ -55,10 +55,10 @@ const encrypt = (data) => {
             padding: CryptoJS.pad.Pkcs7
         });
 
-        const ivHex = iv.toString(CryptoJS.enc.Hex);
-        const ciphertextBase64 = encrypted.ciphertext.toString(CryptoJS.enc.Hex);
+        const ivBase64 = iv.toString(CryptoJS.enc.Base64);
+        const ciphertextBase64 = encrypted.ciphertext.toString(CryptoJS.enc.Base64);
 
-        const payload = ivHex + ":" + ciphertextBase64;
+        const payload = ivBase64 + ":" + ciphertextBase64;
         return payload
     }
 }
