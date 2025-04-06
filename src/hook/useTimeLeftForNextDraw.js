aa6c991c4f8626d0ad9e14fd845f0d2a1fafc8fb
Shirish Koirala <shirish.mjpro@gmail.com>
2025-04-07 09:49:06 +1000

    TimeLeft for next draw fixed.

--- src/hook/useTimeLeftForNextDraw.js
+++ src/hook/useTimeLeftForNextDraw.js
@@ -1,21 +1,55 @@
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect } from "react";
 
 const useTimeLeftForNextDraw = () => {
   const calculateTimeLeft = () => {
     const now = new Date();
-    const nextMondayNineAM = new Date();
+    const options = {
+      timeZone: 'Australia/Sydney',
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: false,
+      weekday: 'short',
+    };
 
-    const dayOfWeek = now.getDay();
+    const parts = new Intl.DateTimeFormat('en-AU', options).formatToParts(now);
+
+    const weekdayString = parts.find(p => p.type === 'weekday').value;
+
+    let dateParts = {};
+    parts.forEach(({ type, value }) => {
+      if (type !== 'literal') {
+        dateParts[type] = value;
+      }
+    });
+
+    const sydneyDateString = `${dateParts.year}-${dateParts.month}-${dateParts.day}T${dateParts.hour}:${dateParts.minute}:${dateParts.second}`;
+
+    const weekdayMapping = {
+      Sun: 0,
+      Mon: 1,
+      Tue: 2,
+      Wed: 3,
+      Thu: 4,
+      Fri: 5,
+      Sat: 6,
+    };
+
+    const nextMondayNineAM = new Date(sydneyDateString);
+
+    const dayOfWeek = weekdayMapping[weekdayString];
     const daysUntilNextMonday = (8 - dayOfWeek) % 7;
-
-    nextMondayNineAM.setDate(now.getDate() + daysUntilNextMonday);
-    nextMondayNineAM.setHours(9, 0, 0, 0);
-
+    nextMondayNineAM.setDate(nextMondayNineAM.getDate() + daysUntilNextMonday);
+    nextMondayNineAM.setHours(0, 0, 0, 0);
     if (dayOfWeek === 1 && now > nextMondayNineAM) {
       nextMondayNineAM.setDate(nextMondayNineAM.getDate() + 7);
     }
 
     const difference = nextMondayNineAM - now;
+
 
     return {
       days: Math.floor(difference / (1000 * 60 * 60 * 24)),
