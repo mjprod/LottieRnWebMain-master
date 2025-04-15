b4817353366437abcddd6321fe2eb212fb3f5d08
Shirish Koirala <shirish.mjpro@gmail.com>
2025-04-15 10:36:48 +1000

    Chevron Down Added.

--- src/templates/TopNavTemplate.jsx
+++ src/templates/TopNavTemplate.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Animated, View, StyleSheet } from "react-native";
 import TopBannerNav from "../components/TopBannerNav";
 import { Colors } from "../util/constants";
@@ -6,26 +6,44 @@ import CopyrightText from "../components/CopyrightText";
 import { useGame } from "../context/GameContext";
 import ProfileHeader from "../components/ProfileHeader";
 import { Platform } from "react-native";
+import LinearGradient from 'react-native-web-linear-gradient';
+import AssetPack from "../util/AssetsPack";
+import LottieView from "react-native-web-lottie";
 
 const TopNavTemplate = ({ title, subtitle, navBackgroudImage, hasBackButton, children, pillText, showCopyright = true, type, showProfileHeader = true }) => {
     const { user } = useGame()
     const scrollY = useRef(new Animated.Value(0)).current;
+    const [bottomChevronStyles, setBottomChevronStyles] = useState(styles.bottomChevronContainer);
 
     const topNavOpacity = scrollY.interpolate({
-        inputRange: [0, 200],
+        inputRange: [0, 160],
         outputRange: [1, 0],
         extrapolate: 'clamp',
     });
 
-    return (
+    const isCloseToBottom = ({ layoutMeasurement, contentOffset, contentSize }) => {
+        const paddingToBottom = 20;
+        return layoutMeasurement.height + contentOffset.y >= contentSize.height - paddingToBottom;
+    };
+
+    const handleScroll = (event) => {
+        Animated.event(
+            [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+            { useNativeDriver: Platform.OS !== 'web', }
+        )
+        if (isCloseToBottom(event.nativeEvent)) {
+            setBottomChevronStyles({ ...bottomChevronStyles, display: 'none' })
+        } else {
+            setBottomChevronStyles({ ...bottomChevronStyles, display: 'flex' });
+        }
+    };
+
+    return (<>
         <Animated.ScrollView
             style={styles.container}
             contentContainerStyle={{ flexGrow: 1 }}
             stickyHeaderIndices={[1]}
-            onScroll={Animated.event(
-                [{ nativeEvent: { contentOffset: { y: scrollY } } }],
-                { useNativeDriver: Platform.OS !== 'web', }
-            )}
+            onScroll={handleScroll}
             scrollEventThrottle={16}
             bounces={false}
             overScrollMode="never">
@@ -42,21 +60,34 @@ const TopNavTemplate = ({ title, subtitle, navBackgroudImage, hasBackButton, chi
             </Animated.View>
             {
                 showProfileHeader &&
-                <Animated.View style={{
+                <Animated.View style={[{
                     backdropFilter: 'blur(10px)',
                     WebkitBackdropFilter: 'blur(10px)',
                     backgroundColor: "#0A0A0AE6",
                     paddingHorizontal: 20,
-                    boxShadow: `0px 6px 10px 2px rgba(0, 0, 0, 0.6)`
-                }}>
+                    boxShadow: `0px 6px 10px 2px rgba(0, 0, 0, 0.6)`,
+                }]}>
                     <Animated.View style={{ borderTopWidth: topNavOpacity, borderTopColor: Colors.jokerBlack200 }}>
                         <ProfileHeader id={user.user_id} name={user.name} />
                     </Animated.View>
                 </Animated.View>
             }
             <View style={showProfileHeader && { paddingTop: 32 }}>{children}</View>
+            {children}
             {showCopyright && <CopyrightText style={{ padding: 20 }} />}
         </Animated.ScrollView>
+        <LinearGradient
+            locations={[0, 1.0]}
+            colors={["#00000000", Colors.jokerBlack1100]}
+            style={bottomChevronStyles}>
+            <LottieView
+                style={styles.lottieAnimation}
+                source={AssetPack.lotties.SCROLL_DOWN_CHEVRON}
+                autoPlay
+                loop={true}
+            />
+        </LinearGradient>
+    </>
     );
 };
 
@@ -64,6 +95,20 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: Colors.jokerBlack1100,
+    },
+    bottomChevronContainer: {
+        position: 'absolute',
+        right: 0,
+        bottom: 0,
+        height: 100,
+        left: 0,
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        paddingBottom: 10,
+    },
+    lottieAnimation: {
+        height: 20,
+        width: 20
     }
 });
 
--- src/util/AssetsPack.js
+++ src/util/AssetsPack.js
@@ -89,7 +89,8 @@ const AssetPack = {
     WIN: require("../assets/lotties/lottieMagicLetterWINheader.json"),
     SPIN_WHEEL: require("../assets/lotties/specialEventsCasinoHomeCarousel.json"),
     CARD_COUNT_DOWN: require("../assets/lotties/lottieCardCountdown.json"),
-    SCRATCHIE_BUBBLE_POP_ERROR : require("../assets/lotties/lottieScratchieBubblePopError.json"),
+    SCRATCHIE_BUBBLE_POP_ERROR: require("../assets/lotties/lottieScratchieBubblePopError.json"),
+    SCROLL_DOWN_CHEVRON: require("../assets/lotties/scroll_down_chevron.json")
   }
 };
 
